e2a3c016a99f37c16d257efa171744e68ee6a7b0
quannt <simpleplan.vn@gmail.com>
2021-04-29 00:38:11 +0900

    Get rid of default images

--- web/index.ts
+++ web/index.ts
@@ -142,16 +142,11 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Quannt', value: 'https://avatars.githubusercontent.com/u/3423859?v=4'}
 ];
 
 const imageDarkOptions: DropdownOption[] = [
-
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Quannt', value: 'https://avatars.githubusercontent.com/u/3423859?v=4'},
 ];
 
 const widthOptions = [
